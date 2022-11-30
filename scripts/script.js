6b4921e2acbaab76400f35786faa50a9c57d4dfb
FatemehGhanizadeh <104738968+FatemehGhanizadeh@users.noreply.github.com>
2022-11-30 23:13:03 +0330

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function()
+{
+    $('.openBtn').click(function(){
+        $('.container').fadeIn(400);
+    })
+
+    $('.close').click(function(){
+        $('.container').fadeOut(400);
+    })
+})
